2a747383c347677071391ee485ef554729503096
Abhishek Jha <abhjha@deloitte.com>
2019-11-06 01:23:44 +0530

    zoneview service integration
    zoneview service integration

--- src/Components/ZoneDatatable/ZoneDatatable.js
+++ src/Components/ZoneDatatable/ZoneDatatable.js
@@ -1,100 +1,98 @@
 import React from 'react';
-import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 export class ZoneDatatable extends React.Component {
 
   constructor(props) {
     super(props);
     this.options = {
-        responsive: true
+      responsive: true
     };
     this.state = {
-        isSearchEnabled: true,
-        isFilterEnabled: true,
-        filterItem : [],
-      
-        zoneName: "",
-        filteredZoneData: {},
-        zoneViewthing:null,
-        
-        
+      isSearchEnabled: true,
+      isFilterEnabled: true,
+      filterItem: [],
+      zoneName: "",
+      filteredZoneData: {},
+      zoneViewthing: null,
     };
   }
-  
-  setStatusStyle(cell, row){
-     let styleClassName = '';
-      if(row.status.toLowerCase() === 'completed'){
-        styleClassName = 'text-danger';
-      } else if(row.status.toLowerCase() === 'in progress'){
-        styleClassName = 'text-primary';
-      } else if(row.status.toLowerCase() ==='waiting'){
-        styleClassName = 'text-warning';
-      }
-      return `<i class='fas fa-circle statusMarker ${ styleClassName }' ></i> ${cell}`; 
+
+  setStatusStyle(cell, row) {
+    let styleClassName = '';
+    if (row.status.toLowerCase() === 'completed') {
+      styleClassName = 'text-danger';
+    } else if (row.status.toLowerCase() === 'in progress') {
+      styleClassName = 'text-primary';
+    } else if (row.status.toLowerCase() === 'waiting') {
+      styleClassName = 'text-warning';
+    }
+    return `<i class='fas fa-circle statusMarker ${styleClassName}' ></i> ${cell}`;
   }
+
   triggerZoneViewTable = () => {
-    fetch('https://iy78q5dt50.execute-api.us-west-2.amazonaws.com/Stage/GetMaterialHistory?zoneId=zone001')
+    fetch(`https://iy78q5dt50.execute-api.us-west-2.amazonaws.com/Stage/GetMaterialHistory?zoneId=${this.props.zoneId}`)
       .then(resp => resp.json())
       .then(response => {
         this.setState({
           filteredZoneData: response,
           zoneName: response.SelectedZone[0].zoneName
         })
-      });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({
+          filteredZoneData:{SelectedZone:[]},
+          zoneName:''
+        })
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.zoneId !== this.props.zoneId) {
+      this.triggerZoneViewTable();
+    }
   }
 
   componentDidMount = () => {
     this.triggerZoneViewTable();
-    
   }
 
+  render() {
+    const { isSearchEnabled } = this.state;
 
- render() {
-    
-   
-    const { isSearchEnabled} = this.state; 
-   
-          return ( 
-            
-          <div id="tableGridPanel">
-            <div className="alert-zone">
-           
-            <div className="alerts-zone-heading">{this.state.zoneName}</div>
-            
-            </div>
-              
-                <div className="tableAndFilterContainer withoutTabs">
-                    <div className="filterIcons">
-                        <i className="fas fa-calendar pull-right tableTools" onClick={this.showHideCalendarTool}></i>       
-                        <i className="fas fa-filter pull-right tableTools" onClick={this.showHideFilterTool}></i> 
-                        <i className="fab fa-sistrix pull-right tableTools" 
-                            onClick={(e) => this.options.showSearchTool(e)}></i>
-                       
-                    </div>
-                   
-                    <input type="hidden" value={this.state.activeTabKey} />
-                   
+    return (
+      <div id="tableGridPanel">
+        <div className="alert-zone">
+          <div className="alerts-zone-heading">{this.state.zoneName}</div>
+        </div>
 
-                            <BootstrapTable 
-                            ref='alertsTable' containerClass="alertsTable" data={this.state.filteredZoneData.SelectedZone}  striped hover bordered={ false } search={isSearchEnabled} multiColumnSearch options={ this.options }> 
-                               <TableHeaderColumn width='80' dataField='statusBox' dataFormat={ this.setStatusStyle} border='0'></TableHeaderColumn>
-                                <TableHeaderColumn width='90' headerAlign='left' dataAlign='center' isKey  dataField='materialName' dataFormat={ this.alertDetails}>Asset</TableHeaderColumn>
-                                <TableHeaderColumn headerAlign='center' dataAlign='center'dataSort >Asset location</TableHeaderColumn>
-                                <TableHeaderColumn headerAlign='center' dataAlign='center'  dataField='status' >Status</TableHeaderColumn>    
-                                <TableHeaderColumn headerAlign='center' dataAlign='center'  dataField='visitTimein' >Zone IN time</TableHeaderColumn>    
-                                <TableHeaderColumn headerAlign='center' dataAlign='center'  dataField='visitTimeout' >Zone OUT time</TableHeaderColumn>    
-                                <TableHeaderColumn headerAlign='center' dataAlign='center'  dataField='description' >Description</TableHeaderColumn>    
-                                <TableHeaderColumn headerAlign='center' dataAlign='center'  dataField='superviserName' >Supervisor</TableHeaderColumn>                         
-                                <TableHeaderColumn headerAlign='center' dataAlign='center'  dataField='activeTime'>Standard Time</TableHeaderColumn>
-                                <TableHeaderColumn headerAlign='center' dataAlign='center'  dataField='description'>Actual Time</TableHeaderColumn>
-
-                              
-                               
-                            </BootstrapTable>
-                </div>
+        <div className="tableAndFilterContainer withoutTabs">
+          <div className="filterIcons">
+            <i className="fas fa-calendar pull-right tableTools" onClick={this.showHideCalendarTool}></i>
+            <i className="fas fa-filter pull-right tableTools" onClick={this.showHideFilterTool}></i>
+            <i className="fab fa-sistrix pull-right tableTools"
+              onClick={(e) => this.options.showSearchTool(e)}></i>
           </div>
+
+          <input type="hidden" value={this.state.activeTabKey} />
+
+          <BootstrapTable
+            ref='alertsTable' containerClass="alertsTable" data={this.state.filteredZoneData.SelectedZone} striped hover bordered={false} search={isSearchEnabled} multiColumnSearch options={this.options}>
+            <TableHeaderColumn width='80' dataField='statusBox' dataFormat={this.setStatusStyle} border='0'></TableHeaderColumn>
+            <TableHeaderColumn width='90' headerAlign='left' dataAlign='center' isKey dataField='materialName' dataFormat={this.alertDetails}>Asset</TableHeaderColumn>
+            <TableHeaderColumn headerAlign='center' dataAlign='center' dataSort >Asset location</TableHeaderColumn>
+            <TableHeaderColumn headerAlign='center' dataAlign='center' dataField='status' >Status</TableHeaderColumn>
+            <TableHeaderColumn headerAlign='center' dataAlign='center' dataField='visitTimein' >Zone IN time</TableHeaderColumn>
+            <TableHeaderColumn headerAlign='center' dataAlign='center' dataField='visitTimeout' >Zone OUT time</TableHeaderColumn>
+            <TableHeaderColumn headerAlign='center' dataAlign='center' dataField='description' >Description</TableHeaderColumn>
+            <TableHeaderColumn headerAlign='center' dataAlign='center' dataField='superviserName' >Supervisor</TableHeaderColumn>
+            <TableHeaderColumn headerAlign='center' dataAlign='center' dataField='activeTime'>Standard Time</TableHeaderColumn>
+            <TableHeaderColumn headerAlign='center' dataAlign='center' dataField='description'>Actual Time</TableHeaderColumn>
+          </BootstrapTable>
+        </div>
+      </div>
     );
   }
- 
 }
 
 export default ZoneDatatable;
--- src/Components/zoneView/zoneView.js
+++ src/Components/zoneView/zoneView.js
@@ -1,73 +1,66 @@
 import React from 'react';
-import {ZoneDatatable} from '../ZoneDatatable/ZoneDatatable';
-
-
+import { ZoneDatatable } from '../ZoneDatatable/ZoneDatatable';
 
 class zoneView extends React.Component {
 
-   
-        constructor(props){
-            super(props)
-            this.state = {
-                colorArray:[ '#44b8e2','#db9bc8','#8ad0f9','#ffde77','#dbdde1'],
-                zoneList: {},
-                zoneID:props.zoneID,   
-                zoneViewthing:null,
-                zoneLength:0
-                
-            }
+    constructor(props) {
+        super(props)
+        this.state = {
+            zoneList: {},
+            zoneId: props.location.state.zoneId,
+            zoneViewthing: null,
+            zoneLength: 0
+
         }
-    
-        triggerZoneViewCardData=()=> {
-           fetch('https://iy78q5dt50.execute-api.us-west-2.amazonaws.com/Stage/GetMaterialCountPerZone')
+    }
+
+    triggerZoneViewCardData = () => {
+        fetch('https://iy78q5dt50.execute-api.us-west-2.amazonaws.com/Stage/GetMaterialCountPerZone')
             .then(resp => resp.json())
             .then(response => {
-                
                 this.setState({
-                zoneList: response,
-                zoneID:response.length>0 && response.map((item) => {
-                    return(item.zoneId)
-
-                }),
-               })
+                    zoneList: response,
+                })
             });
-        }
-       
-        componentDidMount() {
-            this.triggerZoneViewCardData();
-            clearInterval(this.triggerZoneViewCardData);
-            setInterval(this.triggerZoneViewCardData,5000);
-           
-        }
+    }
+
+    triggerTabSelection = (e) => {
+        const zoneId = e.currentTarget.getAttribute('data-id');
+        this.setState({zoneId});
+    }
+
+    componentDidMount() {
+        this.triggerZoneViewCardData();
+        clearInterval(this.triggerZoneViewCardData);
+        setInterval(this.triggerZoneViewCardData, 5000);
+    }
 
     render() {
-       
-        const {zoneList} = this.state;
-        return(
+        const { zoneList, zoneId } = this.state;
+        return (
             <div className="zone-container">
-                <div className = "zone-view">
-                    {zoneList.length > 0 && zoneList.map((item, index)=> {
-                        return (<div className="zone-detail-checkbox">
-                        <div className="upper">
-                        {item.zoneName}
-                        
+                <div className="zone-view">
+                    {zoneList.length > 0 && zoneList.map((item) => {
+                        return (<div className={"zone-detail-checkbox " + (item.zoneId === zoneId ? "active" : "")} data-id={item.zoneId} onClick={this.triggerTabSelection}>
+                            <div className="upper">
+                                {item.zoneName}
+                            </div>
+                            <div className="lower">
+                                {item.count} Assets
                         </div>
-                        <div className="lower" style={{backgroundColor: this.state.colorArray[index]}}>
-                            {item.count} Assets
-                        </div>
-                    </div>)
+                        </div>)
                     })}
                 </div>
-                   
-                <div className="db-alerts card-tile">         
-                        <ZoneDatatable
-                        zoneID={this.state.zoneID}
-                        />
+
+                <div className="db-alerts card-tile">
+                    <ZoneDatatable
+                        zoneId={zoneId}
+                    />
                 </div>
-            </div>        
+            </div>
         );
     }
-  }
+}
 
 export default zoneView;
 
