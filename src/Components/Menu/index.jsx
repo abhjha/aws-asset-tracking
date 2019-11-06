f7c1de86717d29fbe463152b3f9de04ce64bd255
aradhya02 <arasthana@deloitte.com>
2019-11-06 16:28:05 +0530

    menu change done

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -10,7 +10,8 @@ class Menu extends React.Component {
         super(props);
         this.state = {
             selectedMenuId: 0,
-            menuItems: ['Dashboard', 'Asset Registry', 'Device Registry', 'Device Registry'],
+            menuItem:['Dashboard'],
+            // menuItems: ['Dashboard', 'Asset Registry', 'Device Registry', 'Device Registry'],
             // menuItems: ['Dashboard', 'Asset Registry', 'Device Registry', 'Edit Asset','Detach Thing',""],
             // menuItems: ['Dashboard'],
         }
@@ -20,14 +21,15 @@ class Menu extends React.Component {
         if (e.target.parentElement.classList.contains('menu-heading-container')) {
             const selectedMenuId = e.target.parentElement.id;
             this.setState({ selectedMenuId });
-            switch (selectedMenuId) {
-                case "0": this.props.history.push({ pathname: '/' }); break;
-                // case "1": this.props.history.push({ pathname: '/assetRegistry' }); break;
-                // case "2": this.props.history.push({ pathname: '/deviceRegistry' }); break;
-                // case "3": this.props.history.push({ pathname: '/editAsset' }); break;
-                // case "4": this.props.history.push({ pathname: '/detachThing' }); break;
-                default: this.props.history.push({ pathname: '/' });
-            }
+            this.props.history.push({ pathname: '/' });
+            // switch (selectedMenuId) {
+            //     case "0": this.props.history.push({ pathname: '/' }); break;
+            //     // case "1": this.props.history.push({ pathname: '/assetRegistry' }); break;
+            //     // case "2": this.props.history.push({ pathname: '/deviceRegistry' }); break;
+            //     // case "3": this.props.history.push({ pathname: '/editAsset' }); break;
+            //     // case "4": this.props.history.push({ pathname: '/detachThing' }); break;
+            //     default: this.props.history.push({ pathname: '/' });
+            // }
         }
     }
 
@@ -52,7 +54,7 @@ class Menu extends React.Component {
                     MATERIAL VISIBILITY
                 </div>
                 <div onClick={this.setActiveState} className="menu-option-items">
-                    {this.state.menuItems.map((item, index) => {
+                    {this.state.menuItem.map((item, index) => {
                         return (
                             <div id={index} key={index} className={'menu-heading-container ' + (parseInt(this.state.selectedMenuId) === index ? 'active' : '')}>
                                 {/* <img src={logoItems[index]} alt="menu-icons" /> */}
